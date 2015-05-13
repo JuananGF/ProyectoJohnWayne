492be403a0587eb7297fe9e147deda4a2a8055ce
Ange <Ange@AngelAsus-PC>
2015-05-13 19:18:57 +0200

    Java beans de Articulo y Cliente

--- ProyectoJohnWayne/src/Modelo/Articulo.java
+++ ProyectoJohnWayne/src/Modelo/Articulo.java
@@ -2,5 +2,56 @@ package Modelo;
 
 public class Articulo {
 
+	private String nombre;
+	private float precio;
+	static float iva;
+	private String descripcion;
+	
+	//Constructor
+	public Articulo(String nombre, float precio, String descripcion) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripcion = descripcion;
+	}
+	
+	public float calcularPvp(){
+		return (precio * (iva/100));
+	}
+
+	//Getters and setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public static float getIva() {
+		return iva;
+	}
+
+	public static void setIva(float iva) {
+		Articulo.iva = iva;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
 	
 }
--- ProyectoJohnWayne/src/Modelo/Cliente.java
+++ ProyectoJohnWayne/src/Modelo/Cliente.java
@@ -2,4 +2,62 @@ package Modelo;
 
 public class Cliente {
 
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	private int edad;
+	private String colorPelo;
+	
+	//Constructor
+	public Cliente(String nombre, String apellidos, String dni, int edad,
+			String colorPelo) {
+		super();
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.edad = edad;
+		this.colorPelo = colorPelo;
+	}
+
+	//Getters and setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getColorPelo() {
+		return colorPelo;
+	}
+
+	public void setColorPelo(String colorPelo) {
+		this.colorPelo = colorPelo;
+	}
+	
 }
