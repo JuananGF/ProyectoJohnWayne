ef694e95879afeffa20ba3958c76cf86d07fc026
JuananGF <ja_garcia_23@hotmail.com>
2015-05-20 08:57:33 +0200

    lista clientes

--- ProyectoJohnWayne/src/Controlador/ListaClientes.java
+++ ProyectoJohnWayne/src/Controlador/ListaClientes.java
@@ -8,21 +8,29 @@ import javax.swing.JTextField;
 import Modelo.Cliente;
 
 public class ListaClientes {
-	ArrayList<Cliente> cliente=new ArrayList<>();
+	private ArrayList<Cliente> lCliente=new ArrayList<>();
 	JTextField txtNombre;
 	JTextField txtApellidos;
 	JTextField txtDNI;
 	JTextField txtEdad;
 	JTextField txtColorPelo;
 	JComboBox combo;
+	
 	public void cargarLista() {
-		for (int i = 0; i < cliente.size(); i++) {
-			combo.addItem(cliente.get(i).getNombre());
+		for (int i = 0; i < lCliente.size(); i++) {
+			combo.addItem(lCliente.get(i).getNombre());
 		}
 	}
 
 	public void escribirNewElemento() {
-		
-		cliente.add(new Cliente(txtNombre.getText(), txtApellidos.getText(), txtDNI.getText(), Integer.parseInt(txtEdad.getText()), txtColorPelo.getText()));
+		lCliente.add(new Cliente(txtNombre.getText(), txtApellidos.getText(), txtDNI.getText(), Integer.parseInt(txtEdad.getText()), txtColorPelo.getText()));
+	}
+
+	public ArrayList<Cliente> getCliente() {
+		return lCliente;
+	}
+
+	public void setCliente(ArrayList<Cliente> cliente) {
+		this.lCliente = cliente;
 	}
 }
